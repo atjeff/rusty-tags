77dbcdf6c588288d72129373b5b2976b538f0f9f
Jeff Hage <jeffreyhage@gmail.com>
2023-04-22 13:07:11 -0400

    we have some issues with whitespace

--- src/html_element.rs
+++ src/html_element.rs
@@ -1,3 +1,8 @@
+use crate::{
+    parser::{parse_tokens, Parse},
+    tokenizer::{tokenize, HtmlParseError},
+};
+
 #[derive(Debug)]
 pub struct HtmlElement {
     pub tag_name: String,
@@ -9,4 +14,20 @@ pub struct HtmlElement {
 pub struct HtmlAttribute {
     pub name: String,
     pub value: String,
+}
+
+impl Parse for HtmlElement {
+    fn parse(input: &str) -> Result<HtmlElement, HtmlParseError> {
+        let tokens = tokenize(input).unwrap();
+
+        let mut root = HtmlElement {
+            tag_name: String::new(),
+            attributes: Vec::new(),
+            children: Vec::new(),
+        };
+
+        parse_tokens(&mut tokens.into_iter().peekable(), &mut root)?;
+
+        Ok(root.children.pop().unwrap())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,20 @@ mod parser;
 mod tokenizer;
 
 use html_element::HtmlElement;
-use parser::{parse_tokens, Parse};
-use tokenizer::{tokenize, HtmlParseError};
-
-impl Parse for HtmlElement {
-    fn parse(input: &str) -> Result<HtmlElement, HtmlParseError> {
-        let tokens = tokenize(input).unwrap();
-
-        let mut root = HtmlElement {
-            tag_name: String::new(),
-            attributes: Vec::new(),
-            children: Vec::new(),
-        };
-
-        parse_tokens(&mut tokens.into_iter().peekable(), &mut root)?;
-
-        Ok(root.children.pop().unwrap())
-    }
-}
+use parser::Parse;
 
 fn main() {
     // Raw string without escaping quotes
-    let example_input = r#"<div class="container"><p>Hello, world!</p></div>"#;
-    // let example_input = r#"<div class="container"></div>"#;
+    let test_cases = vec![
+        r#"<div class="container"><p>Hello, world!</p></div>"#,
+        r#"<div class="container"></div>"#,
+    ];
 
-    match HtmlElement::parse(&example_input) {
-        Ok(parsed_html) => println!("Parsed HTML: {:#?}", parsed_html),
-        Err(err) => println!("Error parsing HTML: {:#?}", err),
+    // loop through test_cases
+    for test_case in test_cases {
+        match HtmlElement::parse(&test_case) {
+            Ok(parsed_html) => println!("Parsed HTML: {:#?}", parsed_html),
+            Err(err) => println!("Error parsing HTML: {:#?}", err),
+        }
     }
 }
